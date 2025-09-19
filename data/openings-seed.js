2ec6f67b4db91cc22b78f08bad0723e7cfb07656
nextslopellc-lab <nextslopellc@gmail.com>
2025-09-18 23:23:42 -0600

    Create openings-seed.js

---
+++ data/openings-seed.js
@@ -0,0 +1,17 @@
+// data/openings-seed.js
+window.NB_OPENINGS_SEED = {
+  white: {
+    "King's Pawn Game":       ["e4"],
+    "Queen's Pawn Opening":   ["d4"],
+    "Ruy Lopez":              ["e4","e5","Nf3","Nc6","Bb5"],
+    "Italian Game":           ["e4","e5","Nf3","Nc6","Bc4","Bc5"],
+    "English Opening":        ["c4"]
+  },
+  black: {
+    "Sicilian Defense":       ["e4","c5"],
+    "Open Game (1...e5)":     ["e4","e5"],
+    "French Defense":         ["e4","e6","d4","d5"],
+    "Caro-Kann Defense":      ["e4","c6","d4","d5"],
+    "Nimzo-Indian Defense":   ["d4","Nf6","c4","e6","Nc3","Bb4"]
+  }
+};
